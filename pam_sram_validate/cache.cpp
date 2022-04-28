db31d1f0a4256ca2ec419ea2f3b30fc06c30258e
HarryKodden <harry@e-tunity.nl>
2022-04-28 11:36:29 +0000

    Deploying to gh-pages from @ HarryKodden/SRAM-Token-Service@ab3a1ff0f408c8163d6884ec229217fba5d1ef6a 🚀

--- pam_sram_validate/cache.cpp
+++ pam_sram_validate/cache.cpp
@@ -11,86 +11,86 @@
 #include <hiredis/hiredis.h>
 
 class Cache {
-    public:
-        Cache(CONFIG *cfg) {
+	public:
+		Cache(CONFIG *cfg) {
 
-            if (! cfg->redis) {
-                logging(LOG_ERR, "Redis not configured !\n");
-                return;
-            }
+			if (! cfg->redis) {
+				logging(LOG_ERR, "Redis not configured !\n");
+				return;
+			}
 
-            struct timeval timeout = { 1, 500000 }; // 1.5 seconds
-            redis = redisConnectWithTimeout(cfg->redis, atoi(cfg->port), timeout);
+			struct timeval timeout = { 1, 500000 }; // 1.5 seconds
+			redis = redisConnectWithTimeout(cfg->redis, atoi(cfg->port), timeout);
 
-            if (redis == NULL || redis->err) {
-                if (redis) {
-                    logging(LOG_ERR, "Redis Connection error: %s\n", redis->errstr);
-                    redisFree(redis);
-                } else {
-                    logging(LOG_ERR, "Redis Connection error: can't allocate redis context\n");
-                }
-            }
-        }
+			if (redis == NULL || redis->err) {
+				if (redis) {
+					logging(LOG_ERR, "Redis Connection error: %s\n", redis->errstr);
+					redisFree(redis);
+				} else {
+					logging(LOG_ERR, "Redis Connection error: can't allocate redis context\n");
+				}
+			}
+		}
 
-        ~Cache() {
-            if (redis)
-                redisFree(redis);
-        }
+		~Cache() {
+			if (redis)
+				redisFree(redis);
+		}
 
-        void remember(char *key, char *val) {
-            if (! redis)
-                return;
+		void remember(char *key, char *val) {
+			if (! redis)
+				return;
 
-            logging(LOG_ERR, "Remembering: %s...\n", key);
+			logging(LOG_ERR, "Remembering: %s...\n", key);
 
-            void *reply = redisCommand(redis, "SET %s %s", key, val);
-            freeReplyObject(reply);
-        }
+			void *reply = redisCommand(redis, "SET %s %s", key, val);
+			freeReplyObject(reply);
+		}
 
-        bool validate(char *key, char *val) {
-            if (! redis)
-                return false;
+		bool validate(char *key, char *val) {
+			if (! redis)
+				return false;
 
-            redisReply *reply = (redisReply *)redisCommand(redis ,"GET %s", key);
-            if (!reply) {
-                return false;
-            }
+			redisReply *reply = (redisReply *)redisCommand(redis ,"GET %s", key);
+			if (!reply) {
+				return false;
+			}
 
-            if (!reply->str) {
-                logging(LOG_INFO, "Cache key not found: %s\n", key);
-                freeReplyObject(reply);
-                return false;
-            }
+			if (!reply->str) {
+				logging(LOG_INFO, "Cache key not found: %s\n", key);
+				freeReplyObject(reply);
+				return false;
+			}
 
-            bool result = (strcmp(reply->str, val) == 0);
-            freeReplyObject(reply);
-            if (result) {
-                logging(LOG_INFO, "Cache Hit for: %s\n", key);
-            } else {
-                logging(LOG_INFO, "Dropping cache for: %s\n", key);
-                void *reply = redisCommand(redis, "DEL %s", key);
-                freeReplyObject(reply);
-            }
+			bool result = (strcmp(reply->str, val) == 0);
+			freeReplyObject(reply);
+			if (result) {
+				logging(LOG_INFO, "Cache Hit for: %s\n", key);
+			} else {
+				logging(LOG_INFO, "Dropping cache for: %s\n", key);
+				void *reply = redisCommand(redis, "DEL %s", key);
+				freeReplyObject(reply);
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-    private:
-        redisContext *redis = NULL;
+	private:
+		redisContext *redis = NULL;
 };
 
 extern "C"
 {
-    void cache_remember(CONFIG *cfg, char *key, char *val) {
-        Cache *c = new Cache(cfg);
-        c->remember(key, val);
-        delete c;
-    }
+	void cache_remember(CONFIG *cfg, char *key, char *val) {
+		Cache *c = new Cache(cfg);
+		c->remember(key, val);
+		delete c;
+	}
 
-    bool cache_validate(CONFIG *cfg, char *key, char *val) {
-        Cache *c = new Cache(cfg);
-        bool result = c->validate(key, val);
-        delete c;
-        return result;
-    }
+	bool cache_validate(CONFIG *cfg, char *key, char *val) {
+		Cache *c = new Cache(cfg);
+		bool result = c->validate(key, val);
+		delete c;
+		return result;
+	}
 }
--- pam_sram_validate/validate.c
+++ pam_sram_validate/validate.c
@@ -15,103 +15,103 @@
 #include "json.h"
 
 json_value *lookup(json_value* value, const char* name) {
-  json_value * result = NULL;
+	json_value * result = NULL;
 
-  if (value == NULL) {
-    return NULL;
-  }
+	if (value == NULL) {
+		return NULL;
+	}
 
-  char *remaining, *current = strdup(name);
+	char *remaining, *current = strdup(name);
 
-  if ((remaining=strchr(current, '.')) != NULL) {
-	  *remaining++ = '\0';
-  }
+	if ((remaining=strchr(current, '.')) != NULL) {
+		*remaining++ = '\0';
+	}
 
-  for (int x = 0; x < value->u.object.length; x++) {
-    if (!strcmp(value->u.object.values[x].name, current)) {
+	for (int x = 0; x < value->u.object.length; x++) {
+		if (!strcmp(value->u.object.values[x].name, current)) {
 
-      result = (remaining ? lookup(value->u.object.values[x].value, remaining) : value->u.object.values[x].value);
+			result = (remaining ? lookup(value->u.object.values[x].value, remaining) : value->u.object.values[x].value);
 
-      break;
-    }
-  }
+			break;
+		}
+	}
 
-  free(current);
+	free(current);
 
-  return result;
+	return result;
 }
 
 bool validate(CONFIG *cfg, const char *username, const char *token) {
 	char *url =  NULL;
 	char *auth = NULL;
 	char *data = NULL;
-  char *cache_key = NULL;
-  char *cache_value = NULL;
-  bool result = false;
+	char *cache_key = NULL;
+	char *cache_value = NULL;
+	bool result = false;
 
 	if (
-      (asprintf(&url, "%s/api/tokens/introspect", cfg->url) == -1) ||
-		  (asprintf(&auth, "Authorization: Bearer %s", cfg->token) == -1) ||
-		  (asprintf(&data, "token=%s", token) == -1)
-    ) {
+			(asprintf(&url, "%s/api/tokens/introspect", cfg->url) == -1) ||
+			(asprintf(&auth, "Authorization: Bearer %s", cfg->token) == -1) ||
+			(asprintf(&data, "token=%s", token) == -1)
+		) {
 
-    logging(LOG_ERR, "Error allocating memory\n");
-    goto finalize;
+		logging(LOG_ERR, "Error allocating memory\n");
+		goto finalize;
 	}
 
-  cache_key = digest((char *[]) { url, auth, NULL });
-  cache_value = digest((char *[]) { data, NULL });
+	cache_key = digest((char *[]) { url, auth, NULL });
+	cache_value = digest((char *[]) { data, NULL });
 
-  if ((result = cache_validate(cfg, cache_key, cache_value)) == true) {
-    logging(LOG_INFO, "Matched from cache !\n");
-    goto finalize;
-  }
+	if ((result = cache_validate(cfg, cache_key, cache_value)) == true) {
+		logging(LOG_INFO, "Matched from cache !\n");
+		goto finalize;
+	}
 
 	char *response = api(
-    url, "POST", 
-    (char *[]){ "Content-Type: application/x-www-form-urlencoded", auth, NULL}, 
-    data
-  );
+		url, "POST", 
+		(char *[]){ "Content-Type: application/x-www-form-urlencoded", auth, NULL}, 
+		data
+	);
 
-  if (response) {
-    json_value * json = json_parse(response, strlen(response));
-    free(response);
+	if (response) {
+		json_value * json = json_parse(response, strlen(response));
+		free(response);
 
-    if (json) {
-      json_value *active = lookup(json, "active");
+		if (json) {
+			json_value *active = lookup(json, "active");
 
-      if (active && active->u.boolean) {
-        json_value *user = lookup(json, "username");
+			if (active && active->u.boolean) {
+				json_value *user = lookup(json, "username");
 
-        if (user) {
-          result = (strcasecmp(username, user->u.string.ptr) == 0);
+				if (user) {
+					result = (strcasecmp(username, user->u.string.ptr) == 0);
 
-          if (result) {
-            cache_remember(cfg, cache_key, cache_value);
-          } else {
-            	logging(LOG_ERR, "Username %s != %s\n", username, user->u.string.ptr);
-          }
-        } else {
-          logging(LOG_ERR, "Username field not found\n");
-        }
-      } else {
-        logging(LOG_ERR, "Token not active\n");
-      }
+					if (result) {
+						cache_remember(cfg, cache_key, cache_value);
+					} else {
+							logging(LOG_ERR, "Username %s != %s\n", username, user->u.string.ptr);
+					}
+				} else {
+					logging(LOG_ERR, "Username field not found\n");
+				}
+			} else {
+				logging(LOG_ERR, "Token not active\n");
+			}
 
-    	json_value_free(json);
-    } else {
-      logging(LOG_ERR, "Response could not parsed as (valid) json...\n", response);
-    }
-  } else {
-    logging(LOG_ERR, "No response received...\n");
-  }
+			json_value_free(json);
+		} else {
+			logging(LOG_ERR, "Response could not parsed as (valid) json...\n", response);
+		}
+	} else {
+		logging(LOG_ERR, "No response received...\n");
+	}
 
 finalize:
 	free(url);
 	free(auth);
 	free(data);
-  free(cache_key);
-  free(cache_value);
+	free(cache_key);
+	free(cache_value);
 
 	return result;
 }
